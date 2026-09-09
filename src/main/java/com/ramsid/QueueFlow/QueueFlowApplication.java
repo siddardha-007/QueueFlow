8c90158093e97abe2cecd0c65f944a3c46ac0148
siddardha-007 <siddardhabvs@gmail.com>
2026-09-09 11:42:08 +0530

    Initial commit: Spring Boot base project

---
+++ src/main/java/com/ramsid/QueueFlow/QueueFlowApplication.java
@@ -0,0 +1,13 @@
+package com.ramsid.QueueFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramsid/QueueFlow/QueueFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramsid.QueueFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
